540eb1a4ed1b945e1423cba4120ba8ad9cf76183
Ryan Laidlaw <rdl1028@usnh.edu>
2026-02-04 16:44:55 -0500

    use initdao

--- programs/fridge_dao/src/lib.rs
+++ programs/fridge_dao/src/lib.rs
@@ -7,14 +7,11 @@ declare_id!("3Mtgqr9jRqBMAbp9kTde56EWvGhJk744vSqiv9eJmX7q");
 
 #[program]
 pub mod fridge_dao {
-    use super::instructions::*;
+    pub use super::instructions::*;
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitDAO>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
